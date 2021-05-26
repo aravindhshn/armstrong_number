dc89994c0e0b177b0cdd63fb2a756b6ab847338d
aravindh <aravindhshn@gmail.com>
2021-05-26 17:09:13 +0530

    code coverage changes

--- armstrong.py
+++ armstrong.py
@@ -3,21 +3,16 @@
  the index of 3 or not
  for input try numbers 153, 370, 371, 407
 """
-# take input from the user
-num = int(input("Enter a number: "))
+def armstrong_check(num):
+    SUM1 = 0
+    print(num)
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        SUM1 += digit ** 3
+        temp //= 10
 
-# initialize sum
-SUM1 = 0
-
-# find the sum of the cube of each digit
-temp = num
-while temp > 0:
-    digit = temp % 10
-    SUM1 += digit ** 3
-    temp //= 10
-
-# display the result
-if num == SUM1:
-    print(num, "is an Armstrong number.")
-else:
-    print(num, "is not an Armstrong number.")
+    if num == SUM1:
+        return True
+    else:
+        return False
---
+++ testing.py
@@ -0,0 +1,19 @@
+import unittest
+import armstrong
+
+class TestNumber(unittest.TestCase):
+
+    def test_correct_number(self):
+        result = armstrong.armstrong_check(153)
+        self.assertTrue(result)
+
+    def test_wrong_number(self):
+        result = armstrong.armstrong_check(121)
+        self.assertFalse(result)
+    
+    # def test_add(self):
+    #     result = armstrong.add(5, 6)
+    #     self.assertEqual(result, 11)
+
+if __name__ == 'main':
+    unittest.main()
